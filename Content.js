b03070ddc7e22a0654ef2c2a1cb751f4365d9916
ManiPrabhu2005 <pmanikanda447@gmail.com>
2025-08-01 23:22:34 +0530

    Use State
    Use State try

--- Content.js
+++ Content.js
@@ -1,25 +1,41 @@
 import React from 'react'
-
+import { useState } from 'react'
 
 const Content = () => {
+
+
+    const [name, newname] = useState('Mani')
     function handlenamecheck() {
         const names = ['Mani', 'Prabhu', 'Prasath', 'Dhiwa', 'Pradeep']
         const int = Math.floor(Math.random() * 5)
-        return names[int]
+
+        return newname(names[int])
     }
 
-    const handleClick = (name)=>{
-        console.log(`Thanks ${name}`)
-
+    const [count, setCount] = useState(99);
+    function increment() {
+        setCount(count + 1)
     }
-    const handleClick2 = (name)=>{
-        console.log(`Thanks ${name}`)
-
+    function decrement() {
+        setCount(count - 1)
     }
+
+    /*  const handleClick = (name)=>{
+         console.log(`Thanks ${name}`)
+ 
+     }
+     const handleClick2 = (name)=>{
+         console.log(`Thanks ${name}`)
+ 
+     } */
     return (
         <main>
-            <p onDoubleClick={()=>handleClick2('Prabhu')}> Welcome {handlenamecheck()}</p>
-            <button onClick={()=>handleClick('Mani')}>Click</button></main>
+            <p> Welcome {name}</p>
+            <button onClick={handlenamecheck}>Click</button><br></br>
+            <button onClick={decrement}>-</button><br></br>
+            <span>{count}</span><br></br>
+            <button onClick={increment}>+</button>
+        </main>
     )
 
 
