b266158b74b11c558915b37e62d11c069c4a7c4d
ManiPrabhu2005 <pmanikanda447@gmail.com>
2025-08-02 23:59:46 +0530

    To do list
    To do list with css

--- Content.js
+++ Content.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import { useState } from 'react'
+import { FaTrashAlt } from "react-icons/fa";
+import './Content.css';
+
 
 const Content = () => {
 
+    const  [items,setitems]=useState([
+        {
+            id:1,
+            cheked:true,
+            item:"Practice coding"
+        },
+        {
+            id:2,
+            cheked:false,
+            item:"play carrom"
+        },{
+            id:3,
+            cheked:true,
+            item:"study to exam"
+        }
+    ]);
 
+/* 
     const [name, newname] = useState('Mani')
     function handlenamecheck() {
         const names = ['Mani', 'Prabhu', 'Prasath', 'Dhiwa', 'Pradeep']
@@ -19,22 +39,45 @@ const Content = () => {
     function decrement() {
         setCount(count - 1)
     }
-
+ */
     /*  const handleClick = (name)=>{
          console.log(`Thanks ${name}`)
  
      }
      const handleClick2 = (name)=>{
          console.log(`Thanks ${name}`)
- 
-     } */
-    return (
-        <main>
-            <p> Welcome {name}</p>
+
+
+          <p> Welcome {name}</p>
             <button onClick={handlenamecheck}>Click</button><br></br>
             <button onClick={decrement}>-</button><br></br>
             <span>{count}</span><br></br>
             <button onClick={increment}>+</button>
+ 
+     } */
+
+            const handlecheck=(id)=>{
+                    const listitems=items.map((item)=>item.id===id?{...item,cheked:!item.cheked}:item)
+                    setitems(listitems)
+                    }
+
+                const handledelete=(id)=>{
+                    const listitems=items.filter((item)=>item.id
+                    !==id)
+                    setitems(listitems)
+                }
+    return (
+        <main>
+            <ul>
+                {items.map((item)=>(
+                    <li className="item" key={item.id}>
+                        <input type="checkbox" checked={item.cheked} onChange={()=>handlecheck(item.id)}></input>
+                        <label>{item.item}</label>
+                        <FaTrashAlt role='button' tabIndex="0" onClick={()=>handledelete(item.id)}/>
+                    </li>
+                ))}
+            </ul>
+           
         </main>
     )
 
